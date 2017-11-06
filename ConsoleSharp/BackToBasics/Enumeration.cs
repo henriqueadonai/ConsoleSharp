d2a4ef28139bca0cb1f418ac1283e371bd20dbd4
Henrique Adonai <henrique.adonai@hotmail.com>
2017-11-06 14:50:01 -0500

    Add new Enumeration Testing

---
+++ ConsoleSharp/BackToBasics/Enumeration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSharp.BackToBasics
+{
+    public class Enumeration
+    {
+        public enum WorkingDays
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+
+        public void LoopEnumGetNames()
+        {
+            foreach(var item in Enum.GetNames(typeof(WorkingDays)))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public void LoopEnumGetValues()
+        {
+            foreach (var item in Enum.GetValues(typeof(WorkingDays)))
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(Convert.ToInt32(item));
+            }
+            Console.ReadLine();
+        }
+    }
+}
